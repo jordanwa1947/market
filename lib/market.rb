e577e12921327f3c9bb9f8ea7710aafaceec1fd0
Jordan Whitten <jordanwa1947@gmail.com>
2018-09-12 14:11:01 -0600

    working on vendors_that_sell method and tests

--- lib/market.rb
+++ lib/market.rb
@@ -1,7 +1,28 @@
 
 class Market
-  attr_reader :name
+  attr_reader :name, :vendors
   def initialize(name)
     @name = name
+    @vendors = []
+  end
+
+  def add_vendors(vendor)
+    @vendors << vendor
+  end
+
+  def vendor_names
+    @vendors.map do |vendor|
+      vendor.name
+    end
+  end
+
+  def vendors_that_sell(string_search)
+    vendor_inventory = @vendors.group_by do |vendor, items|
+      vendor, items
+    end
+    binding.pry
+    vendor_inverntory.select do |vendor|
+      vendor_items =~ /#{string_search}/
+    end
   end
 end
--- test/market_test.rb
+++ test/market_test.rb
@@ -15,5 +15,54 @@ class MarketTest < Minitest::Test
     market = Market.new("South Pearl Street Farmers Market")
 
     assert_equal "South Pearl Street Farmers Market", market.name
+    assert_equal [], market.vendors
+  end
+
+  def test_market_can_add_vendors
+    market = Market.new("South Pearl Street Farmers Market")
+    vendor_1 = Vendor.new("Rocky Mountain Fresh")
+    vendor_2 = Vendor.new("Ba-Nom-a-Nom")
+    vendor_3 = Vendor.new("Palisade Peach Shack")
+
+    market.add_vendors(vendor_1)
+    market.add_vendors(vendor_2)
+    market.add_vendors(vendor_3)
+
+    expected = market.vendors.all? { |vendor| vendor.class == Vendor}
+
+    assert expected
+    assert_equal 3, market.vendors.count
+  end
+
+  def test_that_market_can_give_an_array_of_names
+    market = Market.new("South Pearl Street Farmers Market")
+    vendor_1 = Vendor.new("Rocky Mountain Fresh")
+    vendor_2 = Vendor.new("Ba-Nom-a-Nom")
+    vendor_3 = Vendor.new("Palisade Peach Shack")
+
+    market.add_vendors(vendor_1)
+    market.add_vendors(vendor_2)
+    market.add_vendors(vendor_3)
+
+    expected = ["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"]
+    assert_equal expected, market.vendor_names
+  end
+
+  def test_that_market_can_search_for_vendors_who_sell_certain_items
+    market = Market.new("South Pearl Street Farmers Market")
+    vendor_1 = Vendor.new("Rocky Mountain Fresh")
+    vendor_2 = Vendor.new("Ba-Nom-a-Nom")
+    vendor_3 = Vendor.new("Palisade Peach Shack")
+
+    market.add_vendors(vendor_1)
+    market.add_vendors(vendor_2)
+    market.add_vendors(vendor_3)
+
+    vendor_1.stock("Banana Nice Cream", 50)
+    vendor_2.stock("Peach-Raspberry Nice Cream", 25)
+    vendor_3.stock("Peaches", 65)
+
+    assert_equal [vendor_2, vendor_3], market.vendors_that_sell('Peaches')
+    assert_equal [vendor_1], market.vendors_that_sell('Peaches')
   end
 end
